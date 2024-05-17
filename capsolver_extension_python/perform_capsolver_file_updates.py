c9e33874b9166c48d1c25527f1e92c632e64d68d
Chetan <53407137+Chetan11-dev@users.noreply.github.com>
2024-05-17 11:22:15 +0530

    fixes

--- capsolver_extension_python/perform_capsolver_file_updates.py
+++ capsolver_extension_python/perform_capsolver_file_updates.py
@@ -1,8 +1,4 @@
-
-
-def perform_capsolver_file_updates(ext, api_key, app_id):
-
-    def update_js_contents(content):
+def update_js_contents(content, api_key, app_id):
         to_replace = "return e.defaultConfig"
         app_id_str = f", appId: '{app_id}'" if app_id else ""
         replacement = (
@@ -10,10 +6,7 @@ def perform_capsolver_file_updates(ext, api_key, app_id):
         )
         return content.replace(to_replace, replacement)
     
-    for file in ext.get_js_files():
-        file.update_contents(update_js_contents)
-    
-    def update_config_contents(content):
+def update_config_contents(content, api_key, app_id):
         key_replaced = content.replace("apiKey: '',", f"apiKey: '{api_key}',")
         # When you use the Capsolver Extension, we integrate our own app ID into the extension if you haven't provided one. This allows us to earn a small commission from each captcha you solve, at no extra cost to you. 
         # This supports us in our efforts to develop awesome open-source projects to make your life easy.
@@ -24,5 +17,9 @@ def perform_capsolver_file_updates(ext, api_key, app_id):
         if app_id:
             key_replaced = key_replaced.replace("appId: '',", f"appId: '{app_id}',")
         return key_replaced
+
+def perform_capsolver_file_updates(ext, api_key, app_id):
+    for file in ext.get_js_files():
+        file.update_contents(lambda x: update_js_contents(x, api_key, app_id))
     
-    ext.get_file("/assets/config.js").update_contents(update_config_contents)
+    ext.get_file("/assets/config.js").update_contents(lambda x: update_config_contents(x, api_key, app_id))
